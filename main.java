55ad0b23f58a6aa1f4ff0fba2c328f481fb95404
BishoyHanna91 <bishoy@pdx.edu>
2018-04-19 14:12:13 -0700

    Java Files for the application

---
+++ main.java
@@ -0,0 +1,30 @@
+/*
+   Name: Bishoy Hanna
+   Date: 3/6/2016
+   Assignment 5
+   number of classes: 10
+  this class is the main which has an object of the manager
+ */
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+public class main {
+    public static void main(String[] ignore) throws IOException {
+
+
+      // creating an object of the manager and call his function
+         manager m = new manager();
+       m.manageAll();
+
+
+     
+
+
+
+
+
+    }
+
+
+}
